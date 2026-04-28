76a3c4ab559c54a5d895f5f91f3c514ff231fd1e
VictorHSCosta <victorhenriquecosta23@gmail.com>
2026-04-28 14:55:07 -0300

    Integrate Redis for Action Cable; update Gemfile and cable configuration, enhance quotes views with current user context

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,6 @@ class Quote < ApplicationRecord
     belongs_to :user
 
     validates :name, presence: true
+
+     after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
 end
